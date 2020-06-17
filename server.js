b807913e4c88d01aad6172646ad51fd54f6b4032
Developer <Dev@jacobs-macbook-pro.home>
2020-06-17 10:11:14 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { connect } = require('./db');
+const api = require("./api");
+const server = express();
+const port = process.env.PORT || 3001;
+
+async function runServer() {
+  await connect();
+  server.use(cors());
+  server.use(api);
+  server.listen(port, () => console.log(`API on port ${port}`));
+}
+
+runServer();
