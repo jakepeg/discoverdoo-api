a8eaf09072a73e6ad4852799e2578f01bb3ae862
Developer <Dev@jacobs-macbook-pro.home>
2020-11-25 06:04:51 +0100

    favourites: type: Array

--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   user: { type: String, required: true, maxlength: 128 },
-  favourites: { type: Boolean, required: false }
+  favourites: { type: Array, required: false }
 })
 
 const User = mongoose.model('User',userSchema);
