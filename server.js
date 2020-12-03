f7eb9ba431067be7d260131c3778591d98cd41aa
Developer <Dev@jacobs-macbook-pro.home>
2020-12-03 13:46:16 +0100

    cors origin

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const api = require("./api");
 const server = express();
 const port = process.env.PORT || 3001;
 
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-// }
+const corsOptions = {
+  origin: 'https://discoverdoo.com',
+}
 
 async function runServer() {
   await connect();
-  server.use(cors());
+  server.use(cors(corsOptions));
   server.use(api);
   server.listen(port, () => console.log(`API on port ${port}`));
 }
