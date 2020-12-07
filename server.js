fe4b476cb908f9e3791b5a02a8f8e564763330c0
Developer <Dev@jacobs-macbook-pro.home>
2020-12-07 14:24:38 +0100

    origin update

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const { connect } = require('./db');
 const api = require("./api");
 const server = express();
 const port = process.env.PORT || 3001;
-// let corsOptions = {}
+let corsOptions = {}
 
-// if (process.env.NODE_ENV === 'production') {
-//   corsOptions = {
-//     origin: 'https://discoverdoo.com',
-//   }
-// } else {
-//   corsOptions = {
-//     origin: 'http://localhost:3000',
-//   }
-// }
-
-const corsOptions = {
+if (port === 3001) {
+  corsOptions = {
+    origin: 'http://localhost:3000',
+  }
+} else {
+  corsOptions = {
     origin: 'https://discoverdoo.com',
   }
+}
+
+// const corsOptions = {
+//   origin: 'https://discoverdoo.com',
+// }
 
 
 async function runServer() {
