b133abaf211112e87e6ac3c18e52eec50653f8f4
Developer <Dev@jacobs-macbook-pro.home>
2020-12-10 16:18:15 +0100

    cors origin auto detect

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { connect } = require('./db');
 const api = require("./api");
 const server = express();
 const port = process.env.PORT || 3001;
-// let corsOptions = {}
+let corsOptions = {}
 
 // if (port === 3001) {
 //   corsOptions = {
@@ -16,15 +16,24 @@ const port = process.env.PORT || 3001;
 //   }
 // }
 
-const corsOptions = {
-  origin: 'https://discoverdoo.com',
+if (process.env.NODE_ENV === 'production') {
+  corsOptions = {
+    origin: 'https://discoverdoo.com',
+  }
+} else {
+  corsOptions = {
+    origin: 'http://localhost:3000',
+  }
 }
+
+
 
 async function runServer() {
   await connect();
   server.use(cors(corsOptions));
   server.use(api);
   server.listen(port, () => console.log(`API on port ${port}`));
+  console.log(corsOptions);
 }
 
 runServer();
